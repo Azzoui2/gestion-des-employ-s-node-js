f8882c6b4cd7022b076415531b0626d9d9107793
Mohammad EL AZZAOUI <mohammadelazzaoui1999@gmail.com>
2024-04-18 11:18:42 +0200

    4/18

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const mongoose = require('mongoose');
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
 });
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
+
+
+mongoose.connect(`mongodb+srv://mohammadelazzaoui1999:${encodeURIComponent("Moha1234@@@@")}@cluster0.z0gc0jf.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0`)
+
+  .then(() => {
+ 
+    app.listen(port, () => {
+      console.log("connexion assure");
+      console.log(`http://localhost:${port}`);
+  });
+    // Tu peux placer ici d'autres actions que tu souhaites exécuter une fois la connexion établie.
+  })
+  .catch((error) => {
+    console.error('Erreur de connexion à la base de données :', error);
+  });
